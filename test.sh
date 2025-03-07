fc1d2d229e45e03e11e8a1bd324af2fb98c97cb3
Fantomasa <ivailoinfo@gmail.com>
2025-03-07 12:07:34 +0200

    edit docker files

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ for i in {1..100}
 do
   echo "Sending transaction $i"
   
-  TX_HASH=$(cast send --rpc-url http://127.0.0.1:8545 \
+  TX_HASH=$(cast send --rpc-url http://localhost:8545 \
     --private-key 0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3 \
     0xFE3B557E8Fb62b89F4916B721be55cEb828dBd73 \
     --value 1ether \
